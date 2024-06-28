7f291b71f07178095bea722bfa6139cf11dedf55
sumitkushwahji <sumitkushwahji@gmail.com>
2024-06-28 12:07:18 +0530

    models changes

--- ntpsync/myapp/models.py
+++ ntpsync/myapp/models.py
@@ -7,14 +7,6 @@ class LogEntry(models.Model):
     ip = models.GenericIPAddressField()
     status = models.CharField(max_length=20)
     bias = models.IntegerField()
-    location = models.ForeignKey(
-        LocationIPMapping, on_delete=models.CASCADE, related_name="log_entries"  # type: ignore
-    )
 
     def __str__(self):
         return f"{self.timestamp} - {self.ip} - {self.status} - {self.location}"
-
-
-class LocationIPMapping(models.Model):
-    location_name = models.CharField(max_length=200)
-    ip_address = models.GenericIPAddressField()
